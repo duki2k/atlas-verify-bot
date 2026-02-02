8963736469a4007b3963ee5e17a34c57ba335dac
Kadu Fuziki <59468628+duki2k@users.noreply.github.com>
2026-02-02 18:03:03 -0300

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -3,24 +3,26 @@ from discord import app_commands
 from discord.ext import commands
 
 from config import load_settings
-from utils.embeds import make_embeds_from_text
+from utils.embeds import make_embeds
 
 settings = load_settings()
+
 
 class AdminCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="ping", description="Teste simples de latência.")
+    @app_commands.command(name="ping", description="Teste simples do bot.")
     async def ping(self, interaction: discord.Interaction) -> None:
-        embeds = make_embeds_from_text(
-            title="Pong 🏓",
-            text=f"{round(self.bot.latency*1000)}ms",
-            emoji_pool=settings.emoji_pool,
-            footer=settings.embed_footer,
+        await interaction.response.defer(ephemeral=True)
+        e = make_embeds(
+            title="🏓 Pong",
+            text=f"{round(self.bot.latency * 1000)}ms",
             color=0x3498DB,
-        )
-        await interaction.response.send_message(embed=embeds[0], ephemeral=True)
+            footer=settings.embed_footer,
+        )[0]
+        await interaction.followup.send(embed=e, ephemeral=True)
+
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(AdminCog(bot))
